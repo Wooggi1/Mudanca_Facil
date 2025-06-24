d141339a5054dae3a658add5db982e5fe04cd984
Thiago Menezes <thiagoprogramador70x7@gmail.com>
2025-06-23 21:03:40 -0300

    pagina inicial feita

--- mudanca_facil_interface/src/pages/Home/Home.tsx
+++ mudanca_facil_interface/src/pages/Home/Home.tsx
@@ -1,13 +1,23 @@
 import Navbar from "../../components/Navbar/Navbar";
-import '../Home/style.css'
+import Button from "../../components/Button/Button";
+import './style.css';
 
 function Home() {
   return (
     <>
       <Navbar />
-      <h1>home</h1>
+      <div className="home-container">
+        <h2 className="title">Mudança fácil</h2>
+        
+        <div className="empty-state">
+          <p className="empty-message">Nenhuma mudança encontrada</p>
+          <Button onClick={() => console.log("Solicitar orçamento clicado")}>
+            Solicitar <br /> orçamento
+          </Button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default Home;
