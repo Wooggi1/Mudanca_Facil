8324e4074cbbc894eae92bc9557b18c79ea82f5a
Thiago Menezes <thiagoprogramador70x7@gmail.com>
2025-06-24 08:15:35 -0300

    feat/OrcamentoCliente_front criado

---
+++ mudanca_facil_interface/src/pages/SucessoOrcamentoCliente/sucessoOrcamentoCliente.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Navbar from '../../components/Navbar/Navbar';
+import Button from '../../components/Button/Button';
+import './SucessoOrcamento.css';
+
+function SucessoOrcamento() {
+  const handleAprovar = () => {
+    alert("Orçamento aprovado!");
+  };
+
+  const handleVerOutros = () => {
+    alert("Redirecionando para outros orçamentos...");
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="sucesso-container">
+        <p className="mensagem">Parabéns...<br />Encontramos essa empresa para você!</p>
+
+        <div className="card-orcamento">
+          <p><strong>Empresa:</strong> Lorem ipsum dolor</p>
+          <p><strong>Avaliação da empresa:</strong> 4 estrelas</p>
+          <p><strong>Valor estimado:</strong> R$ 560,00</p>
+
+          <div className="btn-aprovar">
+            <Button onClick={handleAprovar}>Aprovar orçamento</Button>
+          </div>
+        </div>
+
+        <div className="btn-externo">
+          <Button onClick={handleVerOutros}>Conferir outros orçamentos</Button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default SucessoOrcamento;
