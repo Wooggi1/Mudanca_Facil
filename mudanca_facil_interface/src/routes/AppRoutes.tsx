26f16c8dbd8624c8e9b86d393ec3cf81bf5c7bc0
Thiago <thiago2b9s@gmail.com>
2025-06-24 06:33:54 -0300

    fix: remove rota de testes

--- mudanca_facil_interface/src/routes/AppRoutes.tsx
+++ mudanca_facil_interface/src/routes/AppRoutes.tsx
@@ -3,7 +3,6 @@ import Login from "../pages/Login/Login";
 import RecuperarSenha from "../pages/RecuperarSenha/RecuperarSenha";
 import Cadastro from "../pages/Cadastro/Cadastro";
 import Home from "../pages/Home/Home";
-import SolicitarMudancaModal from "../components/modals/SolicitarMudanca/SolicitarMudaca";
 
 function AppRoutes() {
   return(
@@ -13,7 +12,6 @@ function AppRoutes() {
       <Route path="recuperar" element={<RecuperarSenha />}/>
       <Route path="cadastro" element={<Cadastro />}/>
       <Route path="*" /> // pagina not found
-      <Route path="modal-test" element={<SolicitarMudancaModal />} />
     </Routes>
   )
 }
