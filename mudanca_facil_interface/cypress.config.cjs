d150d0466814b452a57f0f8e091c43e706eb2f00
Thiago <thiago2b9s@gmail.com>
2025-06-24 22:20:18 -0300

    fix: erro de resolucao no cypress

--- mudanca_facil_interface/cypress.config.cjs
+++ mudanca_facil_interface/cypress.config.cjs
@@ -3,5 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
+    viewportWidth: 1280,
+    viewportHeight: 800,
   },
 });
